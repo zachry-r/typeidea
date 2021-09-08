a6301f813b4cd39d58dd8f6e5b3262919a7c3a2a
unknown <361400631@qq.com>
2021-09-08 22:20:50 +0800

    初始化提交

---
+++ typeidea/typeidea/settings/develop.py
@@ -0,0 +1,30 @@
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+from .base import *   #NOQA
+import pymysql
+
+DEBUG=True
+
+pymysql.install_as_MySQLdb()
+DATABASES = {
+    'default':{
+        'ENGINE':'django.db.backends.mysql',
+        'NAME':'django',
+        'USER':'root',
+        'PASSWORD':'123456',
+        'HOST':'localhost',
+        'PORT':'3306'
+    }
+}
+
+
+
+
+
